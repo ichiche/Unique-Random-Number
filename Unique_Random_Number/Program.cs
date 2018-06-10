20555a4b9aa320455650cdc2a714a8e3cd8f4d78
ichiche <ic@eiu.today>
2018-06-10 21:57:53 +0800

    Upload

---
+++ Unique_Random_Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6];
+            int currentRandom;
+            IEnumerable<int> check;
+            Random rd = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(i + "-----");
+                do
+                {
+                    currentRandom = rd.Next(1, 7);
+                    check = arr.Where(item => item == currentRandom);
+                    Console.WriteLine("Random Number: " + currentRandom);
+                } while (check.Count() != 0);
+
+                arr[i] = currentRandom;
+                Console.WriteLine();
+            }
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
